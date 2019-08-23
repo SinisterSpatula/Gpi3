0969fd455312cf9751f9807799df8ee9b43e263f
bebeidon <51832840+bebeidon@users.noreply.github.com>
2019-08-23 03:28:18 +0200

    Update xboxdrvstart.sh

--- xboxdrvstart.sh
+++ xboxdrvstart.sh
@@ -51,6 +51,7 @@ cannonball="--ui-buttonmap back+start=KEY_ESC"
 
 #Scummvm Standalone - select+start=quit, select+LB=Load State, select+RB=Save State.  Select send N for No.  Start sends Enter. Select+X=Keyboard "7"
 scummvm="--ui-buttonmap start=KEY_ENTER,back=KEY_N,back+start=KEY_LEFTCTRL+KEY_Q,back+lb=KEY_LEFTCTRL+KEY_0,back+rb=KEY_LEFTALT+KEY_0,back+x=KEY_7"
+
 #Streets of Rage remake
 sorr="--ui-buttonmap back+start=KEY_ESC"
 
