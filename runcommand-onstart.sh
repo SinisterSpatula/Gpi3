157c798ae8b52f67d734b4053dfef4628991fa41
Adam F <49018201+SinisterSpatula@users.noreply.github.com>
2019-08-23 21:33:43 -0500

    Update runcommand-onstart.sh

--- runcommand-onstart.sh
+++ runcommand-onstart.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+[[ $2 == openbor ]] && filename=$(basename "$3") && cp "/opt/retropie/configs/ports/openbor/Saves/master.cfg" "/opt/retropie/configs/ports/openbor/Saves/${filename%.*}.cfg" 
 source /opt/retropie/configs/all/xboxdrvstart.sh > /dev/null 2>&1
 sudo pkill -STOP mpg123 > /dev/null 2>&1
 fbset -g 320 240 640 480 16
